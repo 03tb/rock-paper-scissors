8c8147ed34e7265867a520f89d384bcdf044375a
03tb <132428099+03tb@users.noreply.github.com>
2023-12-16 23:58:17 +1100

    User input & basic winner calculation coded

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,6 @@ let interimChoice;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
-    console.log(randomNumber);
      if (randomNumber == 0) {
         interimChoice = "rock";
         console.log(interimChoice);
@@ -16,3 +15,30 @@ function getComputerChoice(){
 }
 
 getComputerChoice();
+
+let userSelection = prompt("What's your move? Rock Paper or Scissors?");
+console.log(userSelection);
+
+function whoWon() {
+    if (userSelection == "rock" && interimChoice == "rock") {
+        console.log("It's a draw")
+    } else if (userSelection == "rock" && interimChoice == "scissors") {
+        console.log("You've won!")
+    } else if (userSelection == "rock" && interimChoice == "paper") {
+        console.log("You've lost!")
+    } else if (userSelection == "paper" && interimChoice == "paper") {
+        console.log("It's a draw")
+    } else if (userSelection == "paper" && interimChoice == "scissors") {
+        console.log("You've lost")
+    } else if (userSelection == "paper" && interimChoice == "rock") {
+        console.log("You've won")
+    } else if (userSelection == "scissors" && interimChoice == "scissors") {
+        console.log("It's a draw")
+    } else if (userSelection == "scissors" && interimChoice == "rock") {
+        console.log("You've lost")
+    } else if (userSelection == "scissors" && interimChoice == "paper") {
+        console.log("You've won")
+    }
+}
+
+whoWon();
