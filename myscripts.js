88166d2d2b9c0a03a7bcfbc956e86cda1d58aa66
03tb <132428099+03tb@users.noreply.github.com>
2023-12-17 15:09:11 +1100

    Game completed

--- myscripts.js
+++ myscripts.js
@@ -1,21 +1,20 @@
-let interimChoice;
+// Computer move generator //
+
+let computerSelection;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
      if (randomNumber == 0) {
-        interimChoice = "rock";
-        console.log(interimChoice);
+        computerSelection = "rock";
     } else if (randomNumber == 1) {
-        interimChoice = "paper";
-        console.log(interimChoice);
+        computerSelection = "paper";
     } else {
-        interimChoice = "scissors";
-        console.log(interimChoice);
+        computerSelection = "scissors";
     }
 }
+//*************************************//
 
-// getComputerChoice();
-
+// Selects users move //
 let userSelection;
 let playerSelection;
 
@@ -26,60 +25,75 @@ function getUserChoice() {
     return playerSelection
 }
 
-// getUserChoice();
+//*************************************//
 
+
+// Calculates who won the round & score tracker //
 
 let replay = false;
+let computerScore = 0;
+let playerScore = 0;
 
 function whoWon() {
-    if (playerSelection == "rock" && interimChoice == "rock") {
-        console.log("It's a draw")
-        replay = true;
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        replayRound();
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore = playerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou win!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore = computerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou lose!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        replayRound();
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore = computerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou lose!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore = playerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou win!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        replayRound();
         return replay;
-    } else if (playerSelection == "rock" && interimChoice == "scissors") {
-        console.log("You've won!")
-    } else if (playerSelection == "rock" && interimChoice == "paper") {
-        console.log("You've lost!")
-    } else if (playerSelection == "paper" && interimChoice == "paper") {
-        console.log("It's a draw")
-        replay = true;
-        return replay;
-    } else if (playerSelection == "paper" && interimChoice == "scissors") {
-        console.log("You've lost")
-    } else if (playerSelection == "paper" && interimChoice == "rock") {
-        console.log("You've won")
-    } else if (playerSelection == "scissors" && interimChoice == "scissors") {
-        console.log("It's a draw")
-        replay = true;
-        return replay;
-    } else if (playerSelection == "scissors" && interimChoice == "rock") {
-        console.log("You've lost")
-    } else if (playerSelection == "scissors" && interimChoice == "paper") {
-        console.log("You've won")
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore = computerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou lose!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore = playerScore + 1;
+        alert("You chose: " + userSelection + " the Computer chose: " + computerSelection +"\nYou win!\nPlayer Score: " + playerScore + " Computer Score: " + computerScore);
     }
 }
 
-// whoWon();
 
-function tieBreaker () {
-    replay = false;
-    getComputerChoice();
-    userSelection = prompt("That round was a draw\nRock Paper or Scissors?");
-    playerSelection = userSelection.toLowerCase();
-    console.log(playerSelection);
-    whoWon();
+//*************************************//
+
+//Tie breaker situation function//
+
+function replayRound() {
+        alert("That round was a draw");
+        getComputerChoice();
+        getUserChoice();
+        whoWon();
+        return replay;
 }
 
-if (replay = true) {
-    tieBreaker();
-}
+//*************************************//
 
+//Function to play round//
 function playRound () {
     getComputerChoice();
     getUserChoice();
     whoWon();
 }
 
-playRound();
+//*************************************//
 
-// Need to work on putting all functions into play round function
+function gameLaunch() {
+    while (playerScore <5 && computerScore <5) {
+        playRound();
+    }
+    if (playerScore > computerScore) {
+        alert("Congratulations! You beat the computer!\nFinal Score\nPlayer Score: " + playerScore + " Computer Score: " + computerScore)
+    } else {
+        alert("Game Over! Computer wins!\nFinal Score\nPlayer Score: " + playerScore + " Computer Score: " + computerScore)
+    }
+}
